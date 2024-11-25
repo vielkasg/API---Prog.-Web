b585f3b058f4ee774cc45fc019d4e7c6390a08da
vielkasg <vielkasg5@gmail.com>
2024-11-25 12:49:51 -0400

    Actualizacion

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-function saludar (nombre) {
-    return `Hola, ${nombre}`
-}
+const express = require('express')
+const app = express()
 
-console.log(saludar("Vielka"))
+const {infoCursos} = require('./cursos.js')
 
+//routing
+
+app.get('/', (req, res) => {
+    res.send('Bienvenidos a nuestra API. Cursos disponibles.')
+})
+
+app.get('/api/cursos', (req, res) => {
+    res.send(infoCursos)
+})
+
+const PUERTO = process.env.PORT || 3000
+
+app.listen(PUERTO, () => {
+    console.log(`Este servidor esta escuchando el puerto ${PUERTO}...`)
+})
