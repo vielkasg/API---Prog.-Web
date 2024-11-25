58e3bbe52baa4a9a036ddb9111e56e738ef50797
vielkasg <vielkasg5@gmail.com>
2024-11-25 14:00:49 -0400

    Actualizado

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 
 const {infoCursos} = require('./cursos.js')
 
-//routing
+//routing - rutas
 
 app.get('/', (req, res) => {
     res.send('Bienvenidos a nuestra API. Cursos disponibles.')
@@ -13,6 +13,67 @@ app.get('/api/cursos', (req, res) => {
     res.send(infoCursos)
 })
 
+app.get('/api/cursos/programacion', (req, res) => {
+    res.send(infoCursos.programacion)
+})
+
+app.get('/api/cursos/matematicas', (req, res) => {
+    res.send(infoCursos.matematicas)
+})
+
+//cursos de programacion 
+
+app.get('/api/cursos/programacion/:lenguaje', (req, res) => {
+    const lenguaje = req.params.lenguaje
+    const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje)
+
+    if (resultados.length === 0 ) {
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje}.`)
+    }
+
+    res.send(resultados)
+})
+
+app.get('/api/cursos/programacion/:lenguaje/:nivel', (req, res) => {
+    const lenguaje = req.params.lenguaje
+    const nivel = req.params.nivel
+
+    const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje && curso.nivel === nivel)
+
+    if (resultados.length === 0 ) {
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}.`)
+    }
+
+    res.send(resultados)
+})
+
+//cursos de matematica
+
+app.get('/api/cursos/matematicas/:tema', (req, res) => {
+    const tema = req.params.tema
+    const resultados = infoCursos.matematicas.filter(curso => curso.tema === tema)
+
+    if (resultados.length === 0 ) {
+        return res.status(404).send(`No se encontraron cursos de ${tema}.`)
+    }
+
+    res.send(resultados)
+})
+
+app.get('/api/cursos/matematicas/:tema/:nivel', (req, res) => {
+    const tema = req.params.tema
+    const nivel = req.params.nivel
+
+    const resultados = infoCursos.matematicas.filter(curso => curso.tema === tema && curso.nivel === nivel)
+
+    if (resultados.length === 0 ) {
+        return res.status(404).send(`No se encontraron cursos de ${tema} de nivel ${nivel}.`)
+    }
+
+    res.send(resultados)
+})
+
+//Se define el puerto
 const PUERTO = process.env.PORT || 3000
 
 app.listen(PUERTO, () => {
